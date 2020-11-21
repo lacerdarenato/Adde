6c8a657b9426de95303e1256410a41f90dcbc08f
Renato Lacerda <lacerdarenato00@gmail.com>
2020-11-21 10:20:39 -0300

    Iniciar projeto base

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return jsonify({'text': 'Hello World!'})
+
+
+if __name__ == '__main__':
+    app.run(port=5002)
